d38fbff334e509b67eaa8e68aab4ce9f17c8df90
Ardiansyah Sulistyo <ardiansyahsulistyo@gmail.com>
2024-09-12 09:36:14 +0700

    fix: remove spinner controls from number input field

--- resources/views/admin/books/create.blade.php
+++ resources/views/admin/books/create.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    @vite('resources/css/style.css')
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Tambah Buku') }}
