8efd3683eff267e271089617fe5e814094349de1
Ardiansyah Sulistyo <ardiansyahsulistyo@gmail.com>
2024-09-12 15:30:14 +0700

    chore: adjust focus ring to focus:ring-1 for better visual consistency

--- resources/views/admin/books/index.blade.php
+++ resources/views/admin/books/index.blade.php
@@ -21,7 +21,7 @@
                         </a>
                         <div>
                             <input type="text" id="search" placeholder="Cari Buku"
-                                class="block mt-1 w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring focus:ring-indigo-300 dark:focus:ring-indigo-600 focus:border-indigo-500 dark:focus:border-indigo-600 dark:bg-gray-700">
+                                class="block mt-1 w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-1 focus:ring-indigo-300 dark:focus:ring-indigo-600 focus:border-indigo-500 dark:focus:border-indigo-600 dark:bg-gray-700">
                         </div>
                     </div>
 
