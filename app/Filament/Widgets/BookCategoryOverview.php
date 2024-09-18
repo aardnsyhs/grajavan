714295bb464a43ceb48f12a487435fee959166c0
aardnsyhs <ardiansyahdian755@gmail.com>
2024-09-18 20:29:25 +0700

    feat: add Book Type widgets

--- app/Filament/Widgets/BookCategoryOverview.php
+++ app/Filament/Widgets/BookCategoryOverview.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Book;
+use App\Models\BookType;
 use App\Models\Category;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -14,6 +15,7 @@ class BookCategoryOverview extends BaseWidget
         return [
             Stat::make('Total Buku', Book::count()),
             Stat::make('Total Kategori', Category::count()),
+            Stat::make('Total Tipe Buku', BookType::count()),
         ];
     }
 }
