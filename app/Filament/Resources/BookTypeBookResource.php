6dfc20fd512db1972fc6aceda32e978cf8e707b9
aardnsyhs <ardiansyahdian755@gmail.com>
2024-09-20 09:53:00 +0700

    feat: add view actions

--- app/Filament/Resources/BookTypeBookResource.php
+++ app/Filament/Resources/BookTypeBookResource.php
@@ -66,8 +66,11 @@ class BookTypeBookResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
