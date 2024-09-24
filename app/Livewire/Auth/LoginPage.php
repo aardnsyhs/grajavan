6d93f5b2d225d6e19ad7a046bf8a07e710779450
aardnsyhs <ardiansyahdian755@gmail.com>
2024-09-24 13:54:38 +0700

    feat: Add login form with validation and error handling

--- app/Livewire/Auth/LoginPage.php
+++ app/Livewire/Auth/LoginPage.php
@@ -2,10 +2,30 @@
 
 namespace App\Livewire\Auth;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Login')]
 class LoginPage extends Component
 {
+    public $email;
+    public $password;
+
+    public function save()
+    {
+        $this->validate([
+            'email' => 'required|email|max:255|exists:users,email',
+            'password' => 'required|min:4|max:255'
+        ]);
+
+        if (!auth()->attempt(['email' => $this->email, 'password' => $this->password])) {
+            session()->flash('error', 'Invalid credentials');
+            return;
+        }
+
+        return redirect()->intended();
+    }
+
     public function render()
     {
         return view('livewire.auth.login-page');
--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,21 @@ Route::get('/books', BooksPage::class);
 Route::get('/cart', CartPage::class);
 Route::get('/books/{book_id}', BookDetailPage::class);
 
-Route::get('/checkout', CheckoutPage::class);
-Route::get('/my-orders', MyOrdersPage::class);
-Route::get('/my-orders/{orders}', MyOrderDetailPage::class);
+Route::middleware('guest')->group(function() {
+    Route::get('/login', LoginPage::class);
+    Route::get('/register', RegisterPage::class);
+    Route::get('/forgot', ForgotPasswordPage::class);
+    Route::get('/reset', ResetPasswordPage::class);
+});
 
-Route::get('/login', LoginPage::class);
-Route::get('/register', RegisterPage::class);
-Route::get('/forgot', ForgotPasswordPage::class);
-Route::get('/reset', ResetPasswordPage::class);
-
-Route::get('/success', SuccessPage::class);
-Route::get('/cancel', CancelPage::class);
+Route::middleware('auth')->group(function() {
+    Route::get('/logout', function() {
+        auth()->logout();
+        return redirect('/');
+    });
+    Route::get('/checkout', CheckoutPage::class);
+    Route::get('/my-orders', MyOrdersPage::class);
+    Route::get('/my-orders/{orders}', MyOrderDetailPage::class);
+    Route::get('/success', SuccessPage::class);
+    Route::get('/cancel', CancelPage::class);
+});
