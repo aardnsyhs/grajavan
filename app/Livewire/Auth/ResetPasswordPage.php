48a0774389721e637f76cec35c2e5db42cc2acab
aardnsyhs <ardiansyahdian755@gmail.com>
2024-09-24 14:54:05 +0700

    feat: add reset password and validate

--- app/Livewire/Auth/ResetPasswordPage.php
+++ app/Livewire/Auth/ResetPasswordPage.php
@@ -2,10 +2,57 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Illuminate\Support\Str;
 
+#[Title('Reset Password')]
 class ResetPasswordPage extends Component
 {
+    public $token;
+    #[Url]
+    public $email;
+    public $password;
+    public $password_confirmation;
+
+    public function mount($token)
+    {
+        $this->token = $token;
+    }
+
+    public function save()
+    {
+        $this->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:4|confirmed',
+        ]);
+
+        $status = Password::reset(
+            [
+                'email' => $this->email,
+                'password' => $this->password,
+                'password_confirmation' => $this->password_confirmation,
+                'token' => $this->token,
+            ],
+            function (User $user, string $password) {
+                $password = $this->password;
+                $user->forceFill([
+                    'password' => Hash::make($password),
+                ])->setRememberToken(Str::random(60));
+                $user->save();
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET?redirect('/login'):session()->flash('error', 'Something went wrong');
+    }
+
     public function render()
     {
         return view('livewire.auth.reset-password-page');
