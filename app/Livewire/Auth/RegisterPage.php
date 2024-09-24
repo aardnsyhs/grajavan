0fefbc0499b2b6306f45024f62a6996c99f3d1a2
aardnsyhs <ardiansyahdian755@gmail.com>
2024-09-24 13:39:36 +0700

    feat: Add user registration form with validation

--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -2,10 +2,39 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Register')]
 class RegisterPage extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+
+    // register user
+    public function save()
+    {
+        $this->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:4|max:255',
+        ]);
+
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => $this->password
+        ]);
+
+        // login user
+        auth()->login($user);
+
+        // redirect to home page
+        return redirect()->intended();
+    }
+
     public function render()
     {
         return view('livewire.auth.register-page');
