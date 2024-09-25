b70d875be0177479c973dadd78d4db1ddd016f52
aardnsyhs <ardiansyahdian755@gmail.com>
2024-09-25 09:16:58 +0700

    feat: add proses button

--- resources/views/livewire/checkout-page.blade.php
+++ resources/views/livewire/checkout-page.blade.php
@@ -190,7 +190,8 @@
                 </div>
                 <button type="submit"
                     class="bg-green-500 mt-4 w-full p-3 rounded-lg text-lg text-white hover:bg-green-600">
-                    Buat Pesanan
+                    <span wire:loading.remove>Buat Pesanan</span>
+                    <span wire:loading>Diproses...</span>
                 </button>
                 <div class="bg-white mt-4 rounded-xl shadow p-4 sm:p-7 dark:bg-slate-900">
                     <div class="text-xl font-bold underline text-gray-700 dark:text-white mb-2">
