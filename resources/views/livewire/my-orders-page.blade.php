11746780ab631040908bf083d7482e3a5e5be6f3
aardnsyhs <ardiansyahdian755@gmail.com>
2024-09-25 10:09:08 +0700

    feat(status_payment): add my orders

--- resources/views/livewire/my-orders-page.blade.php
+++ resources/views/livewire/my-orders-page.blade.php
@@ -28,6 +28,43 @@
                         </thead>
                         <tbody>
                             @foreach ($orders as $order)
+                                @php
+                                    $status = '';
+                                    $payment_status = '';
+                                    if ($order->status == 'new') {
+                                        $status = '<span
+                                            class="bg-blue-500 py-1 px-3 rounded text-white shadow">New</span>';
+                                    }
+                                    if ($order->status == 'processing') {
+                                        $status = '<span
+                                            class="bg-yellow-500 py-1 px-3 rounded text-white shadow">Processing</span>';
+                                    }
+                                    if ($order->status == 'shipped') {
+                                        $status = '<span
+                                            class="bg-green-500 py-1 px-3 rounded text-white shadow">Shipped</span>';
+                                    }
+                                    if ($order->status == 'delivered') {
+                                        $status = '<span
+                                            class="bg-green-700 py-1 px-3 rounded text-white shadow">Delivered</span>';
+                                    }
+                                    if ($order->status == 'cancelled') {
+                                        $status = '<span
+                                            class="bg-red-500 py-1 px-3 rounded text-white shadow">Cancelled</span>';
+                                    }
+
+                                    if ($order->payment_status == 'paid') {
+                                        $payment_status = '<span
+                                            class="bg-green-500 py-1 px-3 rounded text-white shadow">Paid</span>';
+                                    }
+                                    if ($order->payment_status == 'pending') {
+                                        $payment_status = '<span
+                                            class="bg-yellow-500 py-1 px-3 rounded text-white shadow">Pending</span>';
+                                    }
+                                    if ($order->payment_status == 'failed') {
+                                        $payment_status = '<span
+                                            class="bg-red-500 py-1 px-3 rounded text-white shadow">Failed</span>';
+                                    }
+                                @endphp
                                 <tr class="odd:bg-white even:bg-gray-100 dark:odd:bg-slate-900 dark:even:bg-slate-800"
                                     wire:key="{{ $order->id }}">
                                     <td
@@ -36,12 +73,10 @@
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
                                         {{ $order->created_at->format('d-m-Y') }}</td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                                        <span
-                                            class="bg-orange-500 py-1 px-3 rounded text-white shadow">{{ $order->status }}</span>
+                                        {!! $status !!}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                                        <span
-                                            class="bg-green-500 py-1 px-3 rounded text-white shadow">{{ $order->payment_status }}</span>
+                                        {!! $payment_status !!}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
                                         {{ Number::currency($order->grand_total, 'IDR') }}</td>
