8495540e7a0ebb974ff8a232273dec4ac4db4b5f
aardnsyhs <ardiansyahdian755@gmail.com>
2024-09-25 11:11:41 +0700

    fix: darkmode support for my order page

--- resources/views/livewire/my-orders-page.blade.php
+++ resources/views/livewire/my-orders-page.blade.php
@@ -1,6 +1,20 @@
+@php
+    $statusClasses = [
+        'new' => 'bg-blue-500 py-1 px-3 rounded text-white shadow',
+        'processing' => 'bg-yellow-500 py-1 px-3 rounded text-white shadow',
+        'shipped' => 'bg-green-500 py-1 px-3 rounded text-white shadow',
+        'delivered' => 'bg-green-700 py-1 px-3 rounded text-white shadow',
+        'cancelled' => 'bg-red-500 py-1 px-3 rounded text-white shadow',
+    ];
+    $paymentStatusClasses = [
+        'paid' => 'bg-green-500 py-1 px-3 rounded text-white shadow',
+        'pending' => 'bg-yellow-500 py-1 px-3 rounded text-white shadow',
+        'failed' => 'bg-red-500 py-1 px-3 rounded text-white shadow',
+    ];
+@endphp
 <div class="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
-    <h1 class="text-4xl font-bold text-slate-500">Pesananku</h1>
-    <div class="flex flex-col bg-white p-5 rounded mt-4 shadow-lg">
+    <h1 class="text-4xl font-bold text-slate-100">Pesananku</h1>
+    <div class="flex flex-col bg-white p-5 rounded mt-4 shadow-lg dark:bg-slate-900">
         <div class="-m-1.5 overflow-x-auto">
             <div class="p-1.5 min-w-full inline-block align-middle">
                 <div class="overflow-hidden">
@@ -23,24 +37,10 @@
                                     class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Jumlah
                                     pesanan</th>
                                 <th scope="col"
-                                    class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Aksi</th>
+                                    class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase">Aksi</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @php
-                                $statusClasses = [
-                                    'new' => 'bg-blue-500 py-1 px-3 rounded text-white shadow',
-                                    'processing' => 'bg-yellow-500 py-1 px-3 rounded text-white shadow',
-                                    'shipped' => 'bg-green-500 py-1 px-3 rounded text-white shadow',
-                                    'delivered' => 'bg-green-700 py-1 px-3 rounded text-white shadow',
-                                    'cancelled' => 'bg-red-500 py-1 px-3 rounded text-white shadow',
-                                ];
-                                $paymentStatusClasses = [
-                                    'paid' => 'bg-green-500 py-1 px-3 rounded text-white shadow',
-                                    'pending' => 'bg-yellow-500 py-1 px-3 rounded text-white shadow',
-                                    'failed' => 'bg-red-500 py-1 px-3 rounded text-white shadow',
-                                ];
-                            @endphp
                             @foreach ($orders as $order)
                                 <tr class="odd:bg-white even:bg-gray-100 dark:odd:bg-slate-900 dark:even:bg-slate-800"
                                     wire:key="{{ $order->id }}">
@@ -69,7 +69,7 @@
                                     <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
                                         <a href="/my-orders/{{ $order->id }}"
                                             class="bg-slate-600 text-white py-2 px-4 rounded-md hover:bg-slate-500">
-                                            View Details
+                                            Lihat Detail
                                         </a>
                                     </td>
                                 </tr>
