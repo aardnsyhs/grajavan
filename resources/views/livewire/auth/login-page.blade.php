320aa596ecc79a46aaf416c5ac7b967fbaeed73a
aardnsyhs <ardiansyahdian755@gmail.com>
2024-09-26 11:19:19 +0700

    refactor: change your email to Email

--- resources/views/livewire/auth/login-page.blade.php
+++ resources/views/livewire/auth/login-page.blade.php
@@ -17,8 +17,8 @@
                     </div>
                 @endif
                 <div>
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                        email</label>
+                    <label for="email"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                     <input type="email" wire:model="email" id="email"
                         class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('email') border-red-500 dark:border-red-500 @enderror"
                         placeholder="johndoe@example.com">
