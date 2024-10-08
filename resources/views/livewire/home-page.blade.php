c4691183a4b0fdd16ec2b43f3f4d03fcca92736b
aardnsyhs <ardiansyahdian755@gmail.com>
2024-10-08 08:32:45 +0700

    refactor: hide button mulai from user authenticated

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -13,23 +13,25 @@
                         novel, biografi, buku anak, buku akademik, hingga literatur klasik dan banyak lagi.</p>
 
                     <!-- Buttons -->
-                    <div class="mt-7 grid gap-3 w-full sm:inline-flex">
-                        <a wire:navigate
-                            class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:bg-blue-500 dark:hover:bg-blue-600"
-                            href="/register">
-                            Mulai
-                            <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24"
-                                height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                stroke-linecap="round" stroke-linejoin="round">
-                                <path d="m9 18 6-6-6-6" />
-                            </svg>
-                        </a>
-                        <a wire:navigate
-                            class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800"
-                            href="/contact">
-                            Hubungi tim sales
-                        </a>
-                    </div>
+                    @guest
+                        <div class="mt-7 grid gap-3 w-full sm:inline-flex">
+                            <a wire:navigate
+                                class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:bg-blue-500 dark:hover:bg-blue-600"
+                                href="/register">
+                                Mulai
+                                <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24"
+                                    height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path d="m9 18 6-6-6-6" />
+                                </svg>
+                            </a>
+                            <a wire:navigate
+                                class="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800"
+                                href="/contact">
+                                Hubungi tim sales
+                            </a>
+                        </div>
+                    @endguest
                     <!-- End Buttons -->
                 </div>
 
