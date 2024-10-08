28dc60d08e0440b8cf62b91615903ddef0102e18
aardnsyhs <ardiansyahdian755@gmail.com>
2024-10-08 08:50:54 +0700

    fix: prevent json_decode() warning by ensuring valid default value for cart_items

--- app/Helpers/CartManagement.php
+++ app/Helpers/CartManagement.php
@@ -104,7 +104,7 @@ class CartManagement {
     // get all cart items from cookie
     static public function getCartItemsFromCookie()
     {
-        $cart_items = json_decode(Cookie::get('cart_items'), true);
+        $cart_items = json_decode(Cookie::get('cart_items', '[]'), true);
         if (!$cart_items) {
             $cart_items = [];
         }
