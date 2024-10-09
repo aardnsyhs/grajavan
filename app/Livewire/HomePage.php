2342eb12b7698909ef1813dc3b30b6507a58d970
aardnsyhs <ardiansyahdian755@gmail.com>
2024-10-09 10:29:08 +0700

    feat: display 4 distinct reviews with rating 5 from different users

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\BookType;
 use App\Models\Category;
+use App\Models\Review;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
@@ -15,9 +16,16 @@ class HomePage extends Component
         $bookTypes = BookType::where('is_active', 1)->get();
         $categories = Category::where('is_active', 1)->get();
 
+        $reviews = Review::with('user')
+        ->where('rating', 5)
+        ->distinct('user_id')
+        ->limit(4)
+        ->get();
+
         return view('livewire.home-page', [
             'bookTypes' => $bookTypes,
-            'categories' => $categories
+            'categories' => $categories,
+            'reviews' =>$reviews
         ]);
     }
 }
